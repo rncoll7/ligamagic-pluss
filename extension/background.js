c6bb0adaf1aeff354f2fe7ce29a9a0a515a226ba
renan <rncoll7@gmail.com>
2022-04-10 02:13:14 -0300

    inject.css

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,12 @@
 chrome.tabs.onUpdated.addListener(function (tab) {
+
 	chrome.scripting.executeScript({
 		target: {tabId: tab, allFrames: true},
 		files: ['inject.js'],
+	});
+
+	chrome.scripting.insertCSS({
+		target: {tabId: tab, allFrames: true},
+		files: ['inject.css'],
 	});
 });
