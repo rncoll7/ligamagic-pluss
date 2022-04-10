68c51c8cd7e465aeaf10e10dc9607cbd669cb4d6
renan <rncoll7@gmail.com>
2022-04-10 02:28:16 -0300

    package suport

--- extension/background.js
+++ extension/background.js
@@ -3,10 +3,10 @@ chrome.tabs.onUpdated.addListener(function (tab) {
 	chrome.scripting.executeScript({
 		target: {tabId: tab, allFrames: true},
 		files: ['inject.js'],
-	});
+	}).then(console.log);
 
 	chrome.scripting.insertCSS({
 		target: {tabId: tab, allFrames: true},
 		files: ['inject.css'],
-	});
+	}).then(console.log);
 });
